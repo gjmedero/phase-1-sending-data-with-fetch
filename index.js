a2f742b9f65b567eb0d0d0897b1c91e5ab20a308
Gilberto Medero <gjmedero@gmail.com>
2023-04-19 17:03:02 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+const data = {
+    name: "Steve",
+    email: "steve@steve.com",
+};
+
+const configObject = {
+    method: "POST",
+    headers: { 
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(data),
+};
+
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", configObject)
+        .then((response) => response.json())
+        .then((object) => {
+            const id = document.createElement('div');
+            id.innerHTML = object.id;
+
+            document.body.appendChild(id);
+        })
+        .catch(function(error) {
+            const err = document.createElement('div');
+            err.innerHTML = error;
+            document.body.appendChild(err);
+        });
+}
